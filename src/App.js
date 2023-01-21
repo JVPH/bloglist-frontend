9f5eeb85e63560647ae7ddd93cac057904574554
JVPH <gyrospizzeria@protonmail.com>
2023-01-21 17:09:19 -0500

    feat: implement login functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -17,9 +22,36 @@ const App = () => {
     fetchBlogs()
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()    
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
   return (
     <div>
       <h2>blogs</h2>
+      {user === null ? 
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username} 
+          password={password} 
+          setUsername={setUsername} 
+          setPassword={setPassword}           
+        /> :
+        <div>
+          <p>{user.name} logged in</p>
+          {/* {noteForm()} */}
+        </div>
+      }
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
