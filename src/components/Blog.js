4f7abf71d42e2d1851bb7978dae44d85e166b9f9
JVPH <gyrospizzeria@protonmail.com>
2023-01-27 22:32:17 -0500

    feat: add 'view' button to each blog
    The button controls whether all of the details about the blog are shown or not.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({blog}) => (
-  <div>
-    <p className="prose-xl">{blog.title} {blog.author}</p>
-  </div>  
-)
+import { useState } from "react"
 
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }  
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>        
+        <p className="prose-xl">{blog.title} {blog.author} <button className='btn btn-ghost' onClick={toggleVisibility}>view</button></p>                        
+      </div>
+      <div className="stats stats-vertical shadow" style={showWhenVisible}>
+        <button className='btn btn-ghost' onClick={toggleVisibility}>hide</button>
+        <div className="stat">
+          <div className="stat-title">Blog:</div>          
+          <p className="prose-xl">{blog.title} {blog.author} </p>
+        </div>
+        <div className="stat">
+          <div className="stat-title">URL:</div>
+          <p className="prose-xl">{blog.url}</p>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Likes:</div>
+          <p className="prose-xl">{blog.likes}</p>
+          <button className="btn btn-sm btn-outline">like</button>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Name:</div>
+          <p className="prose-xl">{blog.user.name}</p>
+        </div>        
+      </div>
+    </div>  
+  )
+
+}
 export default Blog
